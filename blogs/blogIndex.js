ef0a0f802b986f5a5a349bad4ec1485b34f923b5
Jordan Heinrichs <mail@jordanheinrichs.com>
2016-10-29 12:43:35 -0600

    Split out the blogs into their own html files. Changed the sent object for blog list.

---
+++ blogs/blogIndex.js
@@ -0,0 +1,11 @@
+module.exports.hobbyList = {
+   'hiking': { title: 'Hiking', src: 'picture1' },
+   'running': { title: 'Running', src: 'picture2' },
+   'video_games': { title: 'Video Games', src: 'picture3' }
+};
+
+module.exports.projectList = {
+   'solar_car': { title: 'Solar Car', src: 'picture1' },
+   'aero_design': { title: 'Aero Design',  src: 'picture2' }
+};
+
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,27 +1,31 @@
-const project = require('../blogs/projects.js')
-const hobbies = require('../blogs/hobbies.js')
+const blogIndex = require('../blogs/blogIndex.js')
+const path = require('path');
+
+const fileOptions = {
+   root: path.join(__dirname, '../blogs/')
+};
 
 exports.getProjectList = (req, res) => {
-  res.json(project.projectList);
+  res.json(blogIndex.projectList);
 };
 
 exports.getProject = (req, res) => {
-  const projectBlog = project.project[req.params.name];
-  if( projectBlog ) {
-    res.json(projectBlog);
+  const blog = blogIndex.projectList[req.params.name];
+  if( blog ) {
+    res.sendFile(`${req.params.name}.html`, fileOptions);
   } else {
     res.sendStatus(404);
   }
 };
 
 exports.getHobbyList = (req, res) => {
-  res.json(hobbies.hobbyList);
+  res.json(blogIndex.hobbyList);
 };
 
 exports.getHobby = (req, res) => {
-  const blog = hobbies.hobby[req.params.name];
+  const blog = blogIndex.hobbyList[req.params.name];
   if( blog ) {
-    res.json(blog);
+    res.sendFile(`${req.params.name}.html`, fileOptions);
   } else {
     res.sendStatus(404);
   }
