20a1fc017285e7a482a6af1d746845199d468fe8
Jordan Heinrichs <mail@jordanheinrichs.com>
2016-10-29 17:00:26 -0600

    Added images to the blog selectors

--- blogs/blogIndex.js
+++ blogs/blogIndex.js
@@ -1,11 +1,11 @@
 module.exports.hobbyList = {
-   'hiking': { title: 'Hiking', src: 'picture1' },
-   'running': { title: 'Running', src: 'picture2' },
-   'video_games': { title: 'Video Games', src: 'picture3' }
+   'hiking': { title: 'Hiking', src: 'http://suntzu:4000/img/HikingSelector.jpg' },
+   'running': { title: 'Running', src: 'http://suntzu:4000/img/RunningShoesSelector.jpg' },
+   'video_games': { title: 'Video Games', src: 'http://suntzu:4000/img/ComputerSelector.jpg' }
 };
 
 module.exports.projectList = {
-   'solar_car': { title: 'Solar Car', src: 'picture1' },
-   'aero_design': { title: 'Aero Design',  src: 'picture2' }
+   'solar_car': { title: 'Solar Car', src: 'http://suntzu:4000/img/SolarCarSelector.jpg' },
+   'aero_design': { title: 'Aero Design',  src: 'http://suntzu:4000/img/AeroDesignSelector.jpg' }
 };
 
