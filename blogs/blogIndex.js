c67559680d4493f012c1dec4faf4edd6486061ac
Jordan Heinrichs <mail@jordanheinrichs.com>
2018-06-12 00:10:26 -0600

    update endpoint for assets

--- blogs/blogIndex.js
+++ blogs/blogIndex.js
@@ -1,11 +1,11 @@
 module.exports.hobbyList = {
-   'hiking': { title: 'Hiking', src: 'http://104.236.255.20:4000/img/HikingSelector.jpg' },
-   'running': { title: 'Running', src: 'http://104.236.255.20:4000/img/RunningShoesSelector.jpg' },
-   'video_games': { title: 'Video Games', src: 'http://104.236.255.20:4000/img/ComputerSelector.jpg' }
+   'hiking': { title: 'Hiking', src: 'http://167.99.181.149:4000/img/HikingSelector.jpg' },
+   'running': { title: 'Running', src: 'http://167.99.181.149:4000/img/RunningShoesSelector.jpg' },
+   'video_games': { title: 'Video Games', src: 'http://167.99.181.149:4000/img/ComputerSelector.jpg' }
 };
 
 module.exports.projectList = {
-   'solar_car': { title: 'Solar Car', src: 'http://104.236.255.20:4000/img/SolarCarSelector.jpg' },
-   'aero_design': { title: 'Aero Design',  src: 'http://104.236.255.20:4000/img/AeroDesignSelector.jpg' }
+   'solar_car': { title: 'Solar Car', src: 'http://167.99.181.149:4000/img/SolarCarSelector.jpg' },
+   'aero_design': { title: 'Aero Design',  src: 'http://167.99.181.149:4000/img/AeroDesignSelector.jpg' }
 };
 
